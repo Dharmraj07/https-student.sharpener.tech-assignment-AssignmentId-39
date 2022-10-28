6926657f729d2d5976b94963ac5677c4a8387ec5
Dharm Raj Hembram <dharmrajh03@gmail.com>
2022-10-28 20:36:02 +0530

    commit

--- y1.js
+++
@@ -1,4 +0,0 @@
-var itemlist=document.querySelector('#items');
-console.log(itemlist.parentNode);
-itemlist.parentNode.style.backgroundColor="yellow";
-itemlist.parentNode.parentNode.style.backgroundColor="red";
